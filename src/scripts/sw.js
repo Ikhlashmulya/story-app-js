abea3acd8024ecfa383f8ffa006f3cbe5203796f
ikhlashmulya <nurahmanmulya@gmail.com>
2025-05-09 13:10:36 +0700

    add caching

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,3 +1,48 @@
+import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import {
+  CacheFirst,
+  NetworkFirst,
+  StaleWhileRevalidate,
+} from "workbox-strategies";
+import CONFIG from "./config";
+
+const manifest = self.__WB_MANIFEST;
+
+precacheAndRoute(manifest);
+
+registerRoute(
+  ({ url }) => {
+    return (
+      url.origin === "https://cdnjs.cloudflare.com" ||
+      url.origin.includes("fontawesome")
+    );
+  },
+  new CacheFirst({
+    cacheName: "fontawesome-cache",
+  })
+);
+
+registerRoute(
+  ({ request, url }) => {
+    const baseUrl = new URL(CONFIG.BASE_URL);
+    return baseUrl.origin === url.origin && request.destination !== "image";
+  },
+  new NetworkFirst({
+    cacheName: "api-cache",
+  })
+);
+
+registerRoute(
+  ({ request, url }) => {
+    const baseUrl = new URL(CONFIG.BASE_URL);
+    return baseUrl.origin === url.origin && request.destination === "image";
+  },
+  new StaleWhileRevalidate({
+    cacheName: "image-cache",
+  })
+);
+
 self.addEventListener("push", (event) => {
   console.log("Service worker pushing...");
 
